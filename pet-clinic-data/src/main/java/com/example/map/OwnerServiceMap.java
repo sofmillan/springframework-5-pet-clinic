93273639a77708c32db87c844856c2a483067e5e
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-09-13 09:57:38 -0500

    MapService added

--- pet-clinic-data/src/main/java/com/example/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.example.map;
 
 import com.example.model.Owner;
-import com.example.services.CrudService;
+import com.example.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
     public Set<Owner> findAll() {
@@ -30,5 +30,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
